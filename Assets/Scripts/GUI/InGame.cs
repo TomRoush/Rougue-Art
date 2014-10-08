b1dd8fefe0c10a6fcc1ea727957dd03a7b8d4662
Pavle Simonovic <simonov2@illinois.edu>
2014-10-07 23:26:49 -0500

    Added GameState enumerator in Player class
    Added enumerator to allow for easy communication of current game state.

---
+++ Assets/Scripts/GUI/InGame.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InGame {
+
+	static void OnGUI()
+	{
+		if (Player.gameState == Player.GameState.PAUSED) 
+		{
+			GUI.Label (new Rect (50, 50, 75, 75), "PAUSED");
+			Debug.Log("PAUSED");
+			
+		}
+	}
+}
